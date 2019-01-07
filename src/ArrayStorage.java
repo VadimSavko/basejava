629a29c4b61cef4455aeaa1a32f045cd9e03931e
VadimSavko <vadsavgen@mail.ru>
2019-01-08 01:22:02 +0300

    function implementation added : save, get, delete, size, clear, getAll

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,86 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++)
+            if (storage[i] == null)
+                break;
+            else
+                storage[i] = null;
     }
 
     void save(Resume r) {
+        if (r != null)
+            for (int i = 0; i < storage.length; i++) {
+                if (storage[i] != null)
+                    if (storage[i].uuid.equals(r.uuid))
+                        break;
+
+                if (storage[i] == null) {
+                    storage[i] = r;
+                    break;
+                }
+            }
+
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = -1;
+
+        for (int i = 0; i < storage.length; i++)
+            if (storage[i] == null) {
+                break;
+            } else if (storage[i].uuid.equals(uuid)) {
+                index = i;
+                break;
+            }
+
+        if (index != -1)
+            return storage[index];
+        else
+            return null;
     }
 
     void delete(String uuid) {
+        boolean found = false;
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null)
+                break;
+
+            if (!found)
+                found = storage[i].uuid.equals(uuid);
+
+            if (found) {
+                if (i == (storage.length - 1))
+                    storage[i] = null;
+                else
+                    storage[i] = storage[i + 1];
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int size = size();
+        Resume[] result = new Resume[size];
+
+        for (int i = 0; i < size; i++)
+            result[i] = storage[i];
+
+        return result;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+
+        for (Resume r : storage)
+            if (r == null)
+                break;
+            else
+                count++;
+
+        return count;
     }
 }
